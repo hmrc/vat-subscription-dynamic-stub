b68047bb50aa8efb265ba84153ddd1f02b2b701b
Jenkins HMRC <jenkins-hmrc@digital.cabinet-office.gov.uk>
2017-01-16 11:56:11 +0000

    Creating new service capital-gains-subscription-dynamic-stub

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
-
 # capital-gains-subscription-dynamic-stub
 
-[![Build Status](https://travis-ci.org/hmrc/capital-gains-subscription-dynamic-stub.svg?branch=master)](https://travis-ci.org/hmrc/capital-gains-subscription-dynamic-stub) [ ![Download](https://api.bintray.com/packages/hmrc/releases/capital-gains-subscription-dynamic-stub/images/download.svg) ](https://bintray.com/hmrc/releases/capital-gains-subscription-dynamic-stub/_latestVersion)
+[![Build Status](https://travis-ci.org/hmrc/capital-gains-subscription-dynamic-stub.svg)](https://travis-ci.org/hmrc/capital-gains-subscription-dynamic-stub) [ ![Download](https://api.bintray.com/packages/hmrc/releases/capital-gains-subscription-dynamic-stub/images/download.svg) ](https://bintray.com/hmrc/releases/capital-gains-subscription-dynamic-stub/_latestVersion)
 
 This is a placeholder README.md for a new repository
 
 ### License
 
-This code is open source software licensed under the [Apache 2.0 License]("http://www.apache.org/licenses/LICENSE-2.0.html").
-    
+This code is open source software licensed under the [Apache 2.0 License]("http://www.apache.org/licenses/LICENSE-2.0.html")
---
+++ project/MicroServiceBuild.scala
@@ -0,0 +1,33 @@
+import sbt._
+import play.sbt.PlayImport._
+import play.core.PlayVersion
+import uk.gov.hmrc.SbtAutoBuildPlugin
+import uk.gov.hmrc.sbtdistributables.SbtDistributablesPlugin
+import uk.gov.hmrc.versioning.SbtGitVersioning
+
+object MicroServiceBuild extends Build with MicroService {
+
+  val appName = "capital-gains-subscription-dynamic-stub"
+
+  override lazy val appDependencies: Seq[ModuleID] = compile ++ test()
+
+  val compile = Seq(
+    "uk.gov.hmrc" %% "play-reactivemongo" % "5.1.0",
+    ws,
+    "uk.gov.hmrc" %% "microservice-bootstrap" % "5.8.0",
+    "uk.gov.hmrc" %% "play-authorisation" % "4.2.0",
+    "uk.gov.hmrc" %% "play-health" % "2.0.0",
+    "uk.gov.hmrc" %% "play-url-binders" % "2.0.0",
+    "uk.gov.hmrc" %% "play-config" % "3.1.0",
+    "uk.gov.hmrc" %% "logback-json-logger" % "3.1.0",
+    "uk.gov.hmrc" %% "domain" % "4.0.0"
+  )
+
+  def test(scope: String = "test,it") = Seq(
+    "uk.gov.hmrc" %% "hmrctest" % "2.2.0" % scope,
+    "org.scalatest" %% "scalatest" % "2.2.6" % scope,
+    "org.pegdown" % "pegdown" % "1.6.0" % scope,
+    "com.typesafe.play" %% "play-test" % PlayVersion.current % scope
+  )
+
+}
