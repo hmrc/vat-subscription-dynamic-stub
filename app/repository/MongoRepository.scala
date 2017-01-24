d20c4cd417c5f9cdc585ea008a53ef26e6506afb
James Forster <james.forster@digital.hmrc.gov.uk>
2017-01-24 09:57:55 +0000

    Updated types to use correct implicits.

--- app/repository/MongoRepository.scala
+++ app/repository/MongoRepository.scala
@@ -1,14 +1,30 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package repository
 
-import play.api.libs.json.Json
+import play.api.libs.json.Format
 import reactivemongo.api.DB
 import reactivemongo.bson.BSONObjectID
 import uk.gov.hmrc.mongo.{ReactiveRepository, Repository}
 
 import scala.concurrent.{ExecutionContext, Future}
 
-trait CGTMongoConnector[T, O] extends CGTMongoRepository {
-  def apply: CGTMongoRepository
+trait CGTMongoConnector[T, O] extends CGTMongoRepository[T, O] {
+  def apply: CGTMongoRepository[T, O]
 }
 
 trait CGTRepository[T, O] extends Repository[T, BSONObjectID] {
@@ -27,7 +43,6 @@ trait CGTRepository[T, O] extends Repository[T, BSONObjectID] {
 
 }
 
-abstract class CGTMongoRepository[T, O](implicit mongo: () => DB)
-  extends ReactiveRepository[T, BSONObjectID]("protections", mongo, Json.format[T])
-  with CGTRepository[T, O]
-
+abstract class CGTMongoRepository[T, O](implicit mongo: () => DB, formats: Format[T], manifest: Manifest[T])
+  extends ReactiveRepository[T, BSONObjectID]("protections", mongo, formats)
+    with CGTRepository[T, O]
