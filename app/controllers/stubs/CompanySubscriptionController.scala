1e8cec1d8d838de4d1cbb48e5583a80d73d9fdf3
DavidWoodward42 <david.woodward@digital.hmrc.gov.uk>
2017-02-10 15:31:31 +0000

    Added spec and controller to deal with companies.

--- app/controllers/stubs/CompanySubscriptionController.scala
+++ app/controllers/stubs/CompanySubscriptionController.scala
@@ -16,48 +16,81 @@
 
 package controllers.stubs
 
-import actions.SAPExceptionTriggers
 import com.google.inject.{Inject, Singleton}
-import helpers.CGTRefHelper
+import helpers.{CGTRefHelper, CompanyErrorSafeId}
 import models.{CompanySubmissionModel, SubscriberModel}
 import play.api.Logger
 import play.api.libs.json.Json
-import play.api.mvc.{Action, AnyContent}
+import play.api.mvc._
 import repository.SubscriptionMongoConnector
 import uk.gov.hmrc.play.microservice.controller.BaseController
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 @Singleton
 class CompanySubscriptionController @Inject()(subscriptionMongoConnector: SubscriptionMongoConnector,
-                                              cGTRefHelper: CGTRefHelper,
-                                              sAPExceptionTriggers: SAPExceptionTriggers
+                                              cGTRefHelper: CGTRefHelper
                                              ) extends BaseController {
 
-  def subscribe(model: CompanySubmissionModel): Action[AnyContent] = {
 
-    sAPExceptionTriggers.WithCompanySapExceptionTriggers(model).async {
+  def validateBody(model: CompanySubmissionModel): Boolean = {
+    Logger.info("Checking the supplied CompanySubmissionModel has all fields defined")
+    model.sap.isDefined && model.registeredAddress.isDefined && model.contactAddress.isDefined
+  }
+
+  def checkExceptionTriggers(sap: String): Option[Result] = {
+    Logger.info("Checking the supplied sap against the company exception trigger sap's")
+    sap match {
+      case CompanyErrorSafeId.notFound.sap => Some(Results.NotFound(Json.toJson("Not found error")))
+      case CompanyErrorSafeId.badRequest.sap => Some(Results.BadRequest(Json.toJson("Bad request error")))
+      case CompanyErrorSafeId.badGateway.sap => Some(Results.BadGateway(Json.toJson("Bad gateway error")))
+      case CompanyErrorSafeId.internalServerError.sap => Some(Results.InternalServerError(Json.toJson("Internal server error")))
+      case CompanyErrorSafeId.serviceUnavailable.sap => Some(Results.ServiceUnavailable(Json.toJson("Service unavailable error")))
+      case CompanyErrorSafeId.timeout.sap => Some(Results.RequestTimeout(Json.toJson("Timeout error")))
+      case _ =>
+        Logger.info("No exceptions triggered from the supplied sap of: " + sap)
+        None
+    }
+  }
+
+  def returnSubscriptionReference(sap: String): Future[Result] = {
+    val subscriber = subscriptionMongoConnector.repository.findLatestVersionBy(sap)
+
+    def getReference(subscriber: List[SubscriberModel]): Future[String] = {
+      if (subscriber.isEmpty) {
+        val reference = cGTRefHelper.generateCGTReference()
+        Logger.info("Generating a new entry ")
+        subscriptionMongoConnector.repository.addEntry(SubscriberModel(sap, reference))
+        Future.successful(reference)
+      } else {
+        Future.successful(subscriber.head.reference)
+      }
+    }
+
+    for {
+      checkSubscribers <- subscriber
+      reference <- getReference(checkSubscribers)
+    } yield Ok(Json.toJson(reference))
+  }
+
+  def returnBody(model: CompanySubmissionModel): Future[Result] = {
+
+    if (validateBody(model)) {
+      checkExceptionTriggers(model.sap.get) match {
+        case Some(response) => Future.successful(response)
+        case _ => returnSubscriptionReference(model.sap.get)
+      }
+    }
+    else Future.successful(Results.BadRequest(Json.toJson("Body of request did not contain the expected values for the company submission model")))
+  }
+
+  def subscribe(): Action[AnyContent] = {
+    Action.async {
       implicit request => {
-
         Logger.info("Received a call from the back end to subscribe a Company")
-
-        val body = request.body.asJson
-        val sap = body.get.as[CompanySubmissionModel].sap.get
-        val subscriber = subscriptionMongoConnector.repository.findLatestVersionBy(sap)
-
-        def getReference(subscriber: List[SubscriberModel]): Future[String] = {
-          if (subscriber.isEmpty) {
-            val reference = cGTRefHelper.generateCGTReference()
-            subscriptionMongoConnector.repository.addEntry(SubscriberModel(sap, reference))
-            Future.successful(cGTRefHelper.generateCGTReference())
-          } else {
-            Future.successful(subscriber.head.reference)
-          }
-        }
-        for {
-          checkSubscribers <- subscriber
-          reference <- getReference(checkSubscribers)
-        } yield Ok(Json.toJson(reference))
+        val companySubmissionModel = request.body.asJson.get.as[CompanySubmissionModel]
+        returnBody(companySubmissionModel)
       }
     }
   }
---
+++ test/controllers/stubs/CompanySubscriptionControllerSpec.scala
@@ -0,0 +1,197 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.stubs
+
+import javax.inject.Inject
+
+import helpers.CGTRefHelper
+import models.{CompanyAddressModel, CompanySubmissionModel, SubscriberModel}
+import org.mockito.ArgumentMatchers
+import org.mockito.Mockito.when
+import org.scalatest.mock.MockitoSugar
+import play.api.libs.json.Json
+import play.api.mvc.Results
+import play.api.test.Helpers._
+import repository.{SubscriberMongoRepository, SubscriptionMongoConnector}
+import uk.gov.hmrc.play.test.{UnitSpec, WithFakeApplication}
+
+import scala.concurrent.Future
+
+class CompanySubscriptionControllerSpec @Inject()(companySubscriptionController: CompanySubscriptionController)
+  extends UnitSpec with MockitoSugar with WithFakeApplication {
+
+  def setupController(findLatestVersionResult: Future[List[SubscriberModel]], addEntryResult: Future[Unit], ref: String): CompanySubscriptionController = {
+
+    val mockRepository = mock[SubscriberMongoRepository[SubscriberModel, String]]
+    val mockConnector = mock[SubscriptionMongoConnector]
+    val mockCGTRefHelper = mock[CGTRefHelper]
+
+    when(mockConnector.repository)
+      .thenReturn(mockRepository)
+
+    when(mockRepository.addEntry(ArgumentMatchers.any())(ArgumentMatchers.any()))
+      .thenReturn(addEntryResult)
+
+    when(mockRepository.findLatestVersionBy(ArgumentMatchers.any())(ArgumentMatchers.any()))
+      .thenReturn(Future.successful(findLatestVersionResult))
+
+    when(mockCGTRefHelper.generateCGTReference())
+      .thenReturn(ref)
+
+    new CompanySubscriptionController(mockConnector, mockCGTRefHelper)
+  }
+
+  val companyAddressModel = CompanyAddressModel(
+    Some("Line one"),
+    Some("Line two"),
+    Some("Line three"),
+    Some("Line four"),
+    Some("Country"),
+    Some("Postcode")
+  )
+  val noSapCompanySubmissionModel = CompanySubmissionModel(None, Some(companyAddressModel), Some(companyAddressModel))
+  val noRegAddressCompanySubmissionModel = CompanySubmissionModel(Some("dummySap"), None, Some(companyAddressModel))
+  val noCorAddressCompanySubmissionModel = CompanySubmissionModel(Some("dummySap"), Some(companyAddressModel), None)
+  val companySubmissionModel = CompanySubmissionModel(Some("dummySap"), Some(companyAddressModel), Some(companyAddressModel))
+
+  "Calling .validateBody" when {
+
+    "the model has a single empty sap field should return false" in {
+      companySubscriptionController.validateBody(noSapCompanySubmissionModel) shouldEqual false
+    }
+
+    "the model has a single empty registered address field should return false" in {
+      companySubscriptionController.validateBody(noRegAddressCompanySubmissionModel) shouldEqual false
+    }
+
+    "the model has a single empty sap field should return false" in {
+      companySubscriptionController.validateBody(noCorAddressCompanySubmissionModel) shouldEqual false
+    }
+
+    "the model has all fields supplied should return true" in {
+      companySubscriptionController.validateBody(companySubmissionModel) shouldEqual true
+    }
+  }
+
+  "Calling .exceptionTriggers" when {
+
+    "the supplied sap is 003404404 should return" in {
+      companySubscriptionController.checkExceptionTriggers("003404404") shouldEqual Some(Results.NotFound(Json.toJson("Not found error")))
+    }
+
+    "the supplied sap is 003400400 should return" in {
+      companySubscriptionController.checkExceptionTriggers("003400400") shouldEqual Some(Results.BadRequest(Json.toJson("Bad request error")))
+    }
+
+    "the supplied sap is 003500500 should return" in {
+      companySubscriptionController.checkExceptionTriggers("003500500") shouldEqual Some(Results.BadGateway(Json.toJson("Bad gateway error")))
+    }
+
+    "the supplied sap is 003500500 should return" in {
+      companySubscriptionController.checkExceptionTriggers("003502502") shouldEqual Some(Results.InternalServerError(Json.toJson("Internal server error")))
+    }
+
+    "the supplied sap is 003500500 should return" in {
+      companySubscriptionController.checkExceptionTriggers("003503503") shouldEqual Some(Results.ServiceUnavailable(Json.toJson("Service unavailable error")))
+    }
+
+    "the supplied sap is 003500500 should return" in {
+      companySubscriptionController.checkExceptionTriggers("003408408") shouldEqual Some(Results.RequestTimeout(Json.toJson("Timeout error")))
+    }
+
+    "the supplied sap is anything else" should {
+
+      //TODO: This is a perfect place to refactor to use the multiple test suite (Scala check)
+      "return true when called with 192381201" in {
+        companySubscriptionController.checkExceptionTriggers("192381201") shouldEqual None
+      }
+
+      "return true when called with 127514920" in {
+        companySubscriptionController.checkExceptionTriggers("127514920") shouldEqual None
+      }
+
+      "return true when called with 756293710" in {
+        companySubscriptionController.checkExceptionTriggers("756293710") shouldEqual None
+      }
+    }
+  }
+
+  "Calling .returnSubscriptionReference" when {
+
+    "there is an entry in the database for the supplied sap already" should {
+
+      val controller = setupController(Future.successful(List(SubscriberModel("123456789", "CGT123456"))), Future.successful(()), "CGT123456")
+      lazy val result = controller.returnSubscriptionReference("123456789")
+
+      "return a status of 200" in {
+        status(result) shouldBe 200
+      }
+
+      "return a type of Json" in {
+        contentType(result) shouldBe Some("application/json")
+      }
+
+      "return a valid CGT Reference" in {
+        val data = contentAsString(result)
+        val json = Json.parse(data)
+        json.as[String] shouldBe "CGT123456"
+      }
+    }
+
+    "there is no entry in the database for the supplied sap already" should {
+
+      val controller = setupController(Future.successful(List()), Future.successful(()), "CGT654321")
+      lazy val result = controller.returnSubscriptionReference("123456789")
+
+      "return a status of 200" in {
+        status(result) shouldBe 200
+      }
+
+      "return a type of Json" in {
+        contentType(result) shouldBe Some("application/json")
+      }
+
+      "return a valid CGT Reference" in {
+        val data = contentAsString(result)
+        val json = Json.parse(data)
+        json.as[String] shouldBe "CGT654321"
+      }
+    }
+  }
+
+  "Calling .returnBody" when {
+
+    "the body does not have all parameters defined" should {
+
+      val result = companySubscriptionController.returnBody(noSapCompanySubmissionModel)
+
+      "return a status of 400" in {
+        status(result) shouldBe 400
+      }
+
+      "return a type of Json" in {
+        contentType(result) shouldBe Some("application/json")
+      }
+
+      "return a valid CGT Reference" in {
+        val data = contentAsString(result)
+        val json = Json.parse(data)
+        json.as[String] shouldBe "Body of request did not contain the expected values for the company submission model"
+      }
+    }
+  }
+}
