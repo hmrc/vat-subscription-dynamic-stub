0c81e5f25883263ff995043fb68180af85438ab1
Emma-Louise Upton <emma-louise.upton@hmrcaspire.com>
2017-02-16 11:43:01 +0000

    Added obtainDetails function in RegistrationController

--- app/controllers/stubs/RegistrationController.scala
+++ app/controllers/stubs/RegistrationController.scala
@@ -18,11 +18,12 @@ package controllers.stubs
 
 import actions.NinoExceptionTriggersActions
 import javax.inject.{Inject, Singleton}
+
 import helpers.SapHelper
 import models.{BusinessPartnerModel, RegisterModel}
 import play.api.Logger
 import play.api.libs.json.Json
-import play.api.mvc.{Action, AnyContent}
+import play.api.mvc.{Action, AnyContent, Result}
 import repositories.BusinessPartnerRepository
 import uk.gov.hmrc.domain.Nino
 import uk.gov.hmrc.play.microservice.controller.BaseController
@@ -71,6 +72,49 @@ class RegistrationController @Inject()(repository: BusinessPartnerRepository,
           def handleSap(sap: String) = {
             if (sap == "-1")
               Conflict
+            else
+              Ok(Json.toJson(sap))
+          }
+
+          for {
+            bp <- businessPartner
+            sap <- getReference(bp)
+          } yield handleSap(sap)
+        }
+      }
+  }
+
+  val obtainDetails: String => Action[AnyContent] = {
+    nino =>
+      ninoExceptionTriggersActions.WithNinoExceptionTriggers(Nino(nino)).async {
+            //TODO: Update with new error guard when completed
+        implicit request => {
+
+          Logger.warn("Received a call from the back end to retrieve details/SAP for a preexisting business business partner")
+
+          val body = request.body.asJson
+          val registrationDetails = body.get.as[RegisterModel]
+
+          Logger.warn("Opening connection to Mongo")
+
+          val businessPartner: Future[List[BusinessPartnerModel]] = repository().findLatestVersionBy(registrationDetails.nino)
+
+          def getReference(bp: List[BusinessPartnerModel]): Future[String] = {
+
+            Logger.warn("Was passed a list of BPs that were empty? -- " + bp.isEmpty)
+
+            if (bp.isEmpty) {
+              Logger.warn("Failure - making a request to obtain preexisting BP when no BP exists, BAD_REQUEST")
+              Future.successful("-1")
+            } else {
+              Logger.warn("Found an existing entry with sap " + bp.head.sap)
+              Future.successful(bp.head.sap)
+            }
+          }
+
+          def handleSap(sap: String): Result = {
+            if (sap == "-1")
+              BadRequest
             else
               Ok(Json.toJson(sap))
           }
