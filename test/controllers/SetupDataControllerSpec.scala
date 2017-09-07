d0cf4d902df3f41d5055ead2dfc8ac5f7e23e095
samuel-raspin <samuel.raspin@digital.hmrc.gov.uk>
2017-09-07 15:10:47 +0100

    [BTAT-464] - WIP: SetupDataControllerSpec

---
+++ test/controllers/SetupDataControllerSpec.scala
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import akka.util.ByteString
+import org.scalatest.mock.MockitoSugar
+import play.api.libs.json.Json
+import play.mvc.Http.Status
+import uk.gov.hmrc.play.http.HttpResponse
+import uk.gov.hmrc.play.test.{UnitSpec, WithFakeApplication}
+import fakeStuff._
+import models.DataModel
+import play.api.libs.streams.Accumulator
+import play.api.mvc.Result
+import play.api.test.FakeRequest
+import reactivemongo.api.commands.WriteResult
+
+import scala.concurrent.Future
+
+import akka.stream.Materializer
+import play.api.Application
+import play.api.inject.guice.GuiceApplicationBuilder
+
+class SetupDataControllerSpec extends UnitSpec with MockitoSugar with WithFakeApplication with MockSchemaValidation with MockDataRepository{
+
+    val successResponse = HttpResponse(Status.OK, responseJson = Some(Json.parse("{}")))
+
+    //def mockApp: Application = new GuiceApplicationBuilder().build()
+    //val mtrlzr: Materializer = mockApp.injector.instanceOf[Materializer]
+
+    object TestSetupDataController extends SetupDataController(mockSchemaValidation, mockDataRepository)
+
+    "SetupDataController.addData" should {
+
+        val mod: DataModel = DataModel(_id = "1234", schemaId = "2345", method = "7", response = Some(Json.parse("{}")), status = Status.OK)
+
+        lazy val result = TestSetupDataController.addData()(FakeRequest("POST", "/").withJsonBody(Json.parse("""{ "field": "value" }""")))
+
+        "return Status OK (200)" in {
+            mockValidateUrlMatch("1234", "http://something")(response = true)
+            mockValidateResponseJson("1234", None)(response = true)
+            mockLoadUrlRegex("1234")("regex")
+            mockAddEntry(mod)(response = mockWriteResult)
+            writeResultOk()
+            //status(result.run()(mtrlzr)) shouldBe Status.OK
+        }
+
+    }
+
+}
---
+++ test/fakeStuff/MockDataRepository.scala
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fakeStuff
+
+import models.DataModel
+import org.mockito.ArgumentMatchers
+import org.mockito.Mockito.{reset, when}
+import org.scalatest.BeforeAndAfterEach
+import org.scalatest.mock.MockitoSugar
+import repositories.DataRepository
+import uk.gov.hmrc.play.test.UnitSpec
+import reactivemongo.api.commands.WriteResult
+
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
+trait MockDataRepository extends UnitSpec with MockitoSugar with BeforeAndAfterEach {
+
+  val mockDataRepository: DataRepository = mock[DataRepository]
+
+  val mockWriteResult: WriteResult = mock[WriteResult]
+
+  override def beforeEach(): Unit = {
+    super.beforeEach()
+    reset(mockDataRepository)
+  }
+
+  def mockAddEntry(document: DataModel)(response: WriteResult): Unit ={
+    when(mockDataRepository.repository.addEntry(ArgumentMatchers.eq(document)))
+      .thenReturn(Future.successful(response))
+  }
+
+  def writeResultOk(): Unit = {
+    when(mockWriteResult.ok).thenReturn(true)
+  }
+
+}
